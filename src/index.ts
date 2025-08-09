6a4dd5961fe56fd65f7cac768519b18597712d90
ZhiznevskiyArtem <zhiznevskiyartem@gmail.com>
2025-08-09 16:18:44 +0300

    feat: setup prod env

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+import http from 'node:http';
+import 'dotenv/config'
+
+
+const hostname = '127.0.0.1';
+const port = Number(process.env.PORT);
+
+
+const server = http.createServer((request) => {
+
+    console.log(request)
+})
+
+server.listen(port, hostname, () => {
+    console.log()
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
